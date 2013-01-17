4d796fb9a71bea299b1053fef9f6f369c2aef237
galstrom21 <jshepher@rackspace.com>
2013-01-17 16:20:39 -0600

    Initial Import

---
+++ test_runner.py
@@ -0,0 +1,14 @@
+!/usr/bin/env python
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+import unittest2
+import sys
+
+
+if __name__ == '__main__':
+    loader = unittest2.TestLoader()
+    tests = loader.discover('tests')
+    testRunner = unittest2.runner.TextTestRunner(stream=sys.stdout,
+                                                 verbosity=2)
+    runner = testRunner.run(tests)
+    sys.exit(not runner.wasSuccessful())
---
+++ tests/test_example.py
@@ -0,0 +1,23 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+import unittest2
+
+from roushclient.client import RoushEndpoint
+
+class ExampleTestCase(unittest2.TestCase):
+    @classmethod
+    def setUpClass(self):
+        pass
+
+    @classmethod
+    def tearDownClass(self):
+        pass
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_example_test(self):
+        pass
