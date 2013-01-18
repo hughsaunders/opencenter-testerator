b4004b8d9ccc299352320c3802aabdfd1b032163
galstrom21 <jshepher@rackspace.com>
2013-01-18 11:22:45 -0600

    first pass at re-parenting a node

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,14 +1,21 @@
 # vim: tabstop=4 shiftwidth=4 softtabstop=4
 
 import os
+import time
 import unittest2
 
 from roushclient.client import RoushEndpoint
 
+
 class ExampleTestCase(unittest2.TestCase):
+    """
+    This test case assumes a roush-server has been successfully created, and
+    at least 3 nodes have roush-agent installed.
+    """
     @classmethod
     def setUpClass(self):
-        self.endpoint_url = os.environ.get('ROUSH_ENDPOINT', 'http://127.0.0.0:8080')
+        self.endpoint_url = os.environ.get('ROUSH_ENDPOINT',
+                                           'http://127.0.0.0:8080')
         pass
 
     @classmethod
@@ -18,12 +25,26 @@ class ExampleTestCase(unittest2.TestCase):
     def setUp(self):
         self.ep = RoushEndpoint(self.endpoint_url)
         self.admin_ep = RoushEndpoint(self.endpoint_url + '/admin')
+        self.unprovisioned = self.ep.nodes.filter(
+            "name = 'unprovisioned'").first()
 
     def tearDown(self):
         pass
 
     def test_example_test(self):
-        print self.endpoint_url
-        print self.ep
-        print self.admin_ep
+        # Examine all nodes and for every one with a backend of node, set
+        #   their parent_id to the 'unprovisioned' container
+        nodes = self.ep.nodes.filter(
+            "'container' ! in facts.backends")
+        for node in nodes:
+            self.ep.facts.create(node_id=node.id,
+                                 key='parent_id',
+                                 value=self.unprovisioned.id})
+            self.ep.facts.save()
+
+        # sleep for 5 seconds to let the async operations process
+        time.sleep(5)
+        self.ep.nodes._refresh(True)
+        self.ep.facts._refresh(True)
+        print self.ep.facts
         pass
