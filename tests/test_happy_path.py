775c3b9e44d6f190eed945e049b099caaf6ef5c7
galstrom21 <jshepher@rackspace.com>
2013-02-12 15:28:52 -0600

    merge

--- tests/test_happy_path.py
+++ tests/test_happy_path.py
@@ -129,13 +129,13 @@ class ExampleTestCase(unittest2.TestCase):
         new_controller = self.ep.nodes.filter('name = "%s"' % self.controller_name).first()
         self._reparent(new_controller, infra_container)
  	new_controller._request('get')
-        self.assertEquals(new_controller.facts['parent_id'], infra_container.id)	
+        self.assertEquals(new_controller.facts['parent_id'], infra_container.id)
 
 	# Reparent self.controller_name under the new infra container
         new_compute = self.ep.nodes.filter('name = "%s"' % self.compute_name).first()
         self._reparent(new_compute, compute_container)
         new_compute._request('get')
-        self.assertEquals(new_compute.facts['parent_id'], compute_container.id)	
+        self.assertEquals(new_compute.facts['parent_id'], compute_container.id)
 
     def _reparent(self, child_node, parent_node):
         new_fact = self.ep.facts.create(node_id=child_node.id, key='parent_id', value=parent_node.id)
@@ -143,7 +143,7 @@ class ExampleTestCase(unittest2.TestCase):
         self.assertEquals(resp.status_code, 202)
         task = resp.task
         task.wait_for_complete()
-	
+
 
     def _post_new_plan(self, raw_plan, node):
         new_plan = self._update_plan(raw_plan)
